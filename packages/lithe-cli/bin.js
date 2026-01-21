9c82ab095415192735532cd601fb239bab3505dc
lazar <dasidasi86@gmail.com>
2026-01-21 18:10:07 +0200

    new repo structure

---
+++ packages/lithe-cli/bin.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const { spawn } = require('child_process');
+const path = require('path');
+
+// Logic to find the correct binary will go here
+console.log("Lithe CLI wrapper executing...");
